6683435ba50da1e405209e0197f591036f286ca4
Joseph Bailey <46647444+josephlbailey@users.noreply.github.com>
2023-10-06 07:18:16 -0400

    step1: adds AlienInvasion class and refactors

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,32 @@
 import sys
+
 import pygame
 
-pygame.init()
-display = pygame.display.set_mode((800, 600))
-pygame.display.update()
 
-open = True
-while open:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            open = False
-pygame.quit()
+class AlienInvasion:
+    # Overall class to manage game assets and behavior
+
+    def __init__(self):
+        # Initialize the game, and create game resources
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Josephs Alien Invasion")
+
+    def run_game(self):
+        # Start the main loop for the game
+
+        while True:
+            # Watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
 
 quit()
