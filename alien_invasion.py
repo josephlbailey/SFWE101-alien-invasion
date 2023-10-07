8dd7870fe773b4066221d23aed57968a8b67433e
Joseph Bailey <46647444+josephlbailey@users.noreply.github.com>
2023-10-07 15:23:09 -0400

    step10: adds functionality to create multiple rows of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,16 +88,27 @@ class AlienInvasion:
         """ Create the fleet of aliens"""
         # Make a single alien
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
+
         # Determine the available screen space for aliens
         available_space_x = self.settings.screen_width - (2 * alien_width)
         alien_count = available_space_x // (2 * alien_width)
 
-        for alien_number in range(alien_count):
-            alien = Alien(self)
-            alien.x = alien_width + (2 * alien_width * alien_number)
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        row_count = available_space_y // (2 * alien_height)
+
+        for row_number in range(row_count):
+            for alien_number in range(alien_count):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + (2 * alien_width * alien_number)
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + (2 * alien.rect.height * row_number)
+        self.aliens.add(alien)
 
     def _update_screen(self):
         # Redraw the screen each pass through the loop
